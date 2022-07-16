e792b53ab3a3033b2dfef1f213be73e91137a3a8
Aidan <survivalking4@gmail.com>
2022-07-15 22:43:13 -0700

    ykugdluvtf

--- ha_edibles/index.js
+++ ha_edibles/index.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+const https = require("https");
+
+async function apiPostCall(path, jsonData) {
+    return new Promise(function(res, rej) {
+        var data = JSON.stringify(jsonData);
+        var options = {
+            hostname: "aidanjacobson.duckdns.org",
+            port: 443,
+            path: `/api${path}`,
+            method: "POST",
+            headers: {
+                "Content-Type:": "application/json",
+                "Content-Length": data.length
+            }
+        }
+
+        var req = https.request(options, function(res) {
+            res.on("data", function(data) {
+                res(data);
+            });
+        });
+        req.on("error", function(e) {
+            console.error(e);
+            rej(e);
+        })
+        req.write(data);
+        req.end();
+    });
+}
+
+(async function() {
+    console.log(await apiPostCall("/states/edibles", {
+        state: {
+            edibleTimes: []
+        }
+    }))
+})();
